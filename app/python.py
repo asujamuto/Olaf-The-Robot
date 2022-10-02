dc3a90e1dca8be5189221ba5c49d244e9078bcb6
asujamuto <sokolowskipiotr297@protonmail.com>
2022-10-02 23:18:54 +0200

    First Commit

---
+++ app/python.py
@@ -0,0 +1,13 @@
+import sys
+import json
+import ast
+
+
+data_to_pass_back = 'Natalia is the best'
+
+input = ast.literal_eval(sys.argv[1])
+output = input
+output['data_returned'] = data_to_pass_back
+print(json.dumps(output))
+
+sys.stdout.flush()
